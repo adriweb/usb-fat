f68c8326049cf651a53142f11ad9d3ddcbdeb254
MateoConLechuga <matthewwaltzis@gmail.com>
2018-08-04 13:19:43 -0600

    verify volume label is correct

--- test.asm
+++ test.asm
@@ -37,6 +37,10 @@ initFailed:
 #define ld_ix_abc(xx) ld (ix+xx),bc \ ld (ix+xx+3),a
 #define ld_iy_abc(xx) ld (iy+xx),bc \ ld (iy+xx+3),a
 
+BS_VolLab32 = 71
+BPB_FSInfo32 = 48
+MBR_Table = 446
+
 ; mount a logical drive
 ;  z = success
 ; nz = failure
@@ -44,114 +48,160 @@ fs_mount:
 	xor	a,a
 	sbc	hl,hl
 	ld	(xferReadLba),hl
-	ld	(xferReadLba+3),a		; zero lba
-	call	scsiRequestDefaultReadSector	; read the mbr at sector 0
+	ld	(xferReadLba+3),a			; zero lba
+	call	scsiRequestDefaultReadSector		; read the mbr at sector 0
 	call	fs_check
-	cp	a,2				; 2 == no fat filesytem
+	cp	a,2					; 2 = no fat filesytem
 	jr	z,noFatRecord
 	or	a,a
-	jr	z,goodFatRecord			; probably fdisk format here instead if a == 1
-	ld	iy,xferReadDataDefault+446	; offset = mbr table
+	jr	z,goodFatRecord				; probably fdisk format here instead if a == 1
+	ld	iy,xferReadDataDefault+MBR_Table	; offset = mbr table
 	ld	a,(iy+4)
 	or	a,a
-	jr	z,noFatRecord			; type of partition must exist
-	lea	hl,iy+8				; partition offset in lba
+	jr	z,noFatRecord				; type of partition must exist
+	lea	hl,iy+8					; partition offset in lba
 	ld	de,fs_struct+fs_volbase
-	call	util_cp32			; store as little-endian
-	ld	de,xferReadLba			; scsi expects big-endian
+	call	util_cp32				; store as little-endian
+	ld	de,xferReadLba				; scsi expects big-endian
 	call	util_cp32_little_to_big
-	call	scsiRequestDefaultReadSector	; get the fat record sector
+	call	scsiRequestDefaultReadSector		; get the fat record sector
 	call	fs_check
 	or	a,a
-	jr	z,goodFatRecord			; verify valid fat
+	jr	z,goodFatRecord				; verify valid fat
 noFatRecord:
 	xor	a,a
 	inc	a
 	ret
 goodFatRecord:
-	ld	ix,xferReadDataDefault		; sector data
-	ld	iy,fs_struct			; pointer to fat structure
+	ld	ix,xferReadDataDefault			; sector data
+	ld	iy,fs_struct				; pointer to fat structure
+
+	lea	hl,ix + BS_VolLab32			; get the volume label
+	ld	de,OP6
+	push	de
+	call	_Mov11b
+	xor	a,a
+	ld	(OP6+11),a
+	pop	hl
+	debugCall(debugHLStr)				; looks good
+	debugCall(debugNewLine)
 
 	ld	a,(ix+42)
 	or	a,a
 	jr	nz,noFatRecord
 	ld	a,(ix+43)
 	or	a,a
-	jr	nz,noFatRecord			; ensure only support for fat32 v0.0
+	jr	nz,noFatRecord				; ensure only support for fat32 v0.0
 
 	ld_ehl_ix(44)
-	ld_iy_ehl(fs_dirbase)			; root directory start cluster
+	ld_iy_ehl(fs_dirbase)				; root directory start cluster
 	debugCallHexBlock(fs_struct+fs_dirbase, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
 
-	ld_ehl_ix(36)				; euhl = number of sectors per fat
-	ld_iy_ehl(fs_fatsize)			; fs_fasize = number of sectors per fat
+	ld_ehl_ix(36)					; euhl = number of sectors per fat
+	ld_iy_ehl(fs_fatsize)				; fs_fasize = number of sectors per fat
 	debugCallHexBlock(fs_struct+fs_fatsize, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
 
 	xor	a,a
 	ld	bc,0
-	ld	c,(ix+16)			; aubc = number of fats (2 usually)
-	call	__lmulu				; euhl = fs_fasize * aubc
-	ld_iy_ehl(fs_fasize)			; fs_fasize = number of sectors per fat
+	ld	c,(ix+16)				; aubc = number of fats (2 usually)
+	call	__lmulu					; euhl = fs_fasize * aubc
+	ld_iy_ehl(fs_fasize)				; fs_fasize = number of sectors per fat
 	debugCallHexBlock(fs_struct+fs_fasize, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
 
 	xor	a,a
 	ld	bc,0
 	ld	c,(ix+14)
-	ld	b,(ix+15)			; nrsv: number of reserved sectors
+	ld	b,(ix+15)				; nrsv: number of reserved sectors
 	ld	(nrsv),bc
-	call	__ladd				; sysect: nrsv + fasize
+	call	__ladd					; sysect: nrsv + fasize
 	ld	a,e
 	push	hl
 	pop	bc
 	push	bc
 	push	af
-	ld_ehl_ix(32)				; tsect: total number of sectors on the volume
-	call	__lsub				; tsect - sysect
+	ld_ehl_ix(32)					; tsect: total number of sectors on the volume
+	call	__lsub					; tsect - sysect
 	xor	a,a
 	ld	bc,0
 	ld	c,(ix+13)
 	ld	(iy+fs_csize),c
-	call	__ldivu				; nclst: (tsect - sysect) / fs_csize;
+	call	__ldivu					; nclst: (tsect - sysect) / fs_csize;
 	ld	c,2
-	call	__ladd				; num_fatent: nclst + 2
+	call	__ladd					; num_fatent: nclst + 2
 	ld_iy_ehl(fs_num_fatent)
 	debugCallHexBlock(fs_struct+fs_num_fatent, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
 
 	ld_ehl_iy(fs_volbase)
-nrsv =$+1
+	debugCallHexBlock(fs_struct+fs_volbase, 4)
+	debugCall(debugNewLine)				; looks good
+
 	ld	bc,0
+nrsv =$-3
 	xor	a,a
 	call	__ladd
-	ld_iy_ehl(fs_fatbase)			; fatbase: volbase + nrsv
+	ld_iy_ehl(fs_fatbase)				; fatbase: volbase + nrsv
 	debugCallHexBlock(fs_struct+fs_fatbase, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
 
 	ld_ehl_iy(fs_volbase)
 	pop	af
-	pop	bc				; aubc = sysect
+	pop	bc					; aubc = sysect
 	call	__ladd
-	ld_iy_ehl(fs_database)			; database: volbase + sysect
+	ld_iy_ehl(fs_database)				; database: volbase + sysect
 	debugCallHexBlock(fs_struct+fs_database, 4)
-	debugCall(debugNewLine)			; looks good
+	debugCall(debugNewLine)				; looks good
+
+	ld	a,(ix + BPB_FSInfo32)			; get filesystem information if available
+	cp	a,1
+	jr	nz,nofileinfo
+
+	ld_ehl_iy(fs_volbase)
+	inc	hl					; fs_volbase + 1 -- probably unsafe to do this
+
+	call	fs_move_window
+
+nofileinfo:
 
 	xor	a,a
 	ld	(fs_struct+fs_flag),a
 	ret
 
-; opens a file to the pointer
-; hl = name 8.3
-fs_open:
+; move to different sector
+; inputs:
+;  euhl: sector
+; outputs:
+;
+fs_move_window:
 	ret
 
-; hl = path string to file or directory
-;  z = success
-; nz = fail
-follow_path:
-	ret
+
+	; static FRESULT move_window (	/* Returns FR_OK or FR_DISK_ERR */
+	; 	FATFS* fs,			/* Filesystem object */
+	; 	DWORD sector		/* Sector number to make appearance in the fs->win[] */
+	; )
+	; {
+	; 	FRESULT res = FR_OK;
+        ;
+        ;
+	; 	if (sector != fs->winsect) {	/* Window offset changed? */
+	; #if !FF_FS_READONLY
+	; 		res = sync_window(fs);		/* Write-back changes */
+	; #endif
+	; 		if (res == FR_OK) {			/* Fill sector window with new data */
+	; 			if (disk_read(fs->pdrv, fs->win, sector, 1) != RES_OK) {
+	; 				sector = 0xFFFFFFFF;	/* Invalidate window if read data is not valid */
+	; 				res = FR_DISK_ERR;
+	; 			}
+	; 			fs->winsect = sector;
+	; 		}
+	; 	}
+	; 	return res;
+	; }
+
 
 ; builds a name based on the file input segment from 8.3 format
 create_name:
@@ -252,6 +302,7 @@ util_zero:
 ; filesystem handling
 ; ---
 
+; filesystem object structure
 fs_struct:
 fs_type =$-fs_struct
 	.db	0	; fat type
@@ -283,6 +334,14 @@ fs_curr_clust =$-fs_struct
 	.db	0,0,0,0	; file current cluster
 fs_dsect =$-fs_struct
 	.db	0,0,0,0	; file current data sector
+fs_last_clst =$-fs_struct
+	.db	0,0,0,0	; last allocated cluster
+fs_free_clst =$-fs_struct
+	.db	0,0,0,0	; number of free clusters
+fs_fsi_flag =$-fs_struct
+	.db	0	; file system information flag
+fs_winsect:
+	.db	0,0,0,0 ; current visible sector
 
 ; ---
 ; directory handling
